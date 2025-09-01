cc1f12b99f3028434f7ed84a5d3e8094ab2307ce
Vyacheslav <relaxerrkis@gmail.com>
2025-09-01 23:26:49 +0400

    added assets, script

---
+++ Assets/Scripts/Controllers/TerrainGenerator.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+  private List<GameObject> terrainObjects;
+
+  private void Start()
+  {
+    
+  }
+}
